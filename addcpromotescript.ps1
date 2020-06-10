2894354ece47d4389eb6de456b87e7597e0cd3a6
Bruce M. Cosden <bruce@cosden.me>
2020-06-10 16:06:58 -0400

    update script

--- addcpromotescript.ps1
+++ addcpromotescript.ps1
@@ -48,11 +48,9 @@ Try
     Write-Log 'Install ADDS'
 
     $domainuser = $vmuser + "@" + $addcdomain
-    Write-Log $domainuser
-    Write-Log $vmpassword
 
     #Create Secondary ADDC
-    Install-ADDSDomainController -CriticalReplicationOnly -CreateDnsDelegation:$false -Credential (New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $domainuser, (ConvertTo-SecureString -String $vmpassword -AsPlainText -Force)) -DatabasePath "F:\NTDS" -LogPath "F:\NTDS" -SysvolPath "F:\SYSVOL" -DomainName $addcdomain -InstallDns:$true -SafeModeAdministratorPassword (ConvertTo-SecureString -AsPlainText $vmpassword -Force) -NoRebootOnCompletion -Force:$true
+    Install-ADDSDomainController -CriticalReplicationOnly -CreateDnsDelegation:$false -Credential (New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $domainuser, (ConvertTo-SecureString -AsPlainText $vmpassword -Force)) -DatabasePath "F:\NTDS" -LogPath "F:\NTDS" -SysvolPath "F:\SYSVOL" -DomainName $addcdomain -InstallDns:$true -SafeModeAdministratorPassword (ConvertTo-SecureString -AsPlainText $vmpassword -Force) -NoRebootOnCompletion -Force:$true
     Write-Log 'Create Forest'
 
     New-ADReplicationSubnet -Name $subnet_addc -Site $defaultsitename
