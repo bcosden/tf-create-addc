fe42536cd0bcb11cdcfc52d584686f59fddb722f
= <=>
2020-06-11 17:22:59 -0400

    updated

--- addcpromotescript.ps1
+++ addcpromotescript.ps1
@@ -30,6 +30,10 @@ Write-Log($message) {
 
 Try
 {
+    Write-Log 'Sleeping....'
+    Start-Sleep -s 300
+    Write-Log 'Waking up....'
+
     #Assign RAW disk and add drive letter
     $addisk = Get-Disk -Number 2
     Write-Log 'Get-Disk'
@@ -55,8 +59,8 @@ Try
     $cred = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $domainuser, $pword
 
     #Create Secondary ADDC
-    Install-ADDSDomainController -CriticalReplicationOnly -ADPrepCredential $cred -CreateDnsDelegation:$false -Credential $cred -DatabasePath "F:\NTDS" -LogPath "F:\NTDS" -SysvolPath "F:\SYSVOL" -DomainName $addcdomain -InstallDns:$true -SafeModeAdministratorPassword (ConvertTo-SecureString -AsPlainText $vmpassword -Force) -NoRebootOnCompletion -SkipPreChecks -Force:$true
-    Write-Log 'Create Forest'
+    Install-ADDSDomainController -CriticalReplicationOnly -CreateDnsDelegation:$false -Credential $cred -DatabasePath "F:\NTDS" -LogPath "F:\NTDS" -SysvolPath "F:\SYSVOL" -DomainName $addcdomain -InstallDns:$true -SafeModeAdministratorPassword (ConvertTo-SecureString -AsPlainText $vmpassword -Force) -NoRebootOnCompletion -Force:$true
+    Write-Log 'Added Domain Controller'
 
     New-ADReplicationSubnet -Name $subnet_addc -Site $defaultsitename
     Write-Log 'Create Default Site Name'
