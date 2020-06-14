0305e4dcd412b0996f99b4289ab792ca2af20f36
Bruce Cosden <bruce@cosden.me>
2020-06-14 18:19:15 -0400

    added windows var set

---
+++ tfvars.ps1
@@ -0,0 +1,5 @@
+$env:ARM_SUBSCRIPTION_ID=(az keyvault secret show --name tf-subscription-id --vault-name az-mgmt-keyvault --query value -o tsv)
+$env:ARM_CLIENT_ID=(az keyvault secret show --name tf-client-id --vault-name az-mgmt-keyvault --query value -o tsv)
+$env:ARM_CLIENT_SECRET=(az keyvault secret show --name tf-client-secret --vault-name az-mgmt-keyvault --query value -o tsv)
+$env:ARM_TENANT_ID=(az keyvault secret show --name tf-tenant-id --vault-name az-mgmt-keyvault --query value -o tsv)
+$env:ARM_ACCESS_KEY=(az keyvault secret show --name tf-access-key --vault-name az-mgmt-keyvault --query value -o tsv)
